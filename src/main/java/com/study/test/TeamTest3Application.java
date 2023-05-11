e51a4923f00d6f7b9ae02c59cc2e1e531ddf7813
InyoungHa <qhdla1004t@gmail.com>
2023-05-11 14:53:37 +0900

    프로젝트 생성

---
+++ src/main/java/com/study/test/TeamTest3Application.java
@@ -0,0 +1,13 @@
+package com.study.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTest3Application.class, args);
+	}
+
+}
